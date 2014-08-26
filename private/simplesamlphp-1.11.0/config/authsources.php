999598a82d6c63bb792c3962d1b765b12a500859
Ariel Gold <ari@getpantheon.com>
2014-08-25 17:13:48 -0700

    Modify simplesaml authsources.php per quickstart: https://simplesamlphp.org/docs/1.9/simplesamlphp-sp#section_1_1

--- private/simplesamlphp-1.11.0/config/authsources.php
+++ private/simplesamlphp-1.11.0/config/authsources.php
@@ -15,14 +15,15 @@ $config = array(
 	// and Shibboleth 1.3 IdPs.
 	'default-sp' => array(
 		'saml:SP',
-
+    'privatekey' => 'saml.pem',
+    'certificate' => 'saml.crt',
 		// The entity ID of this SP.
 		// Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
 		'entityID' => NULL,
 
 		// The entity ID of the IdP this should SP should contact.
 		// Can be NULL/unset, in which case the user will be shown a list of available IdPs.
-		'idp' => NULL,
+		'idp' => 'https://openidp.feide.no',
 
 		// The URL to the discovery service.
 		// Can be NULL/unset, in which case a builtin discovery service will be used.
